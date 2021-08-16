e196602e9ecdab392bb7b3e9f1112e3f51191ba3
Irvin Cruz <irvincruz912@gmail.com>
2021-08-16 11:28:55 -0500

    FEAT: Se crea poryecto

---
+++ src/test/java/mx/santander/fiduciarioplus/lib/exception/ExceptionLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.santander.fiduciarioplus.lib.exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
